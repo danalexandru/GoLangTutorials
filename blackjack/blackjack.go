9a85029391d92c90b48d65232b8da709a549b27d
= <danb.alexandru@gmail.com>
2020-09-01 13:13:10 +0300

    [add]   - added the "DrawCard" in "blackjack.go"   - added the "TestDrawCard" method in "blackjack_test.go"

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -4,6 +4,11 @@ import deck "../deckofcards"
 
 // GenerateBlackjackDeck ...
 // This method returns a number of decks concatenated together to be used in the blackjack game
+// Parameters:
+// - numberOfDecks: (int) the number of decks containing distinct 52 cards
+// Return:
+// - ([]deck.Card) a bigger deck containing "numberOfDecks" decks concatenated together
+// - (Error) if an error occurred
 func GenerateBlackjackDeck(numberOfDecks int) ([]deck.Card, error) {
 	deckOfCards := []deck.Card{}
 
@@ -20,4 +25,17 @@ func GenerateBlackjackDeck(numberOfDecks int) ([]deck.Card, error) {
 	deck.ShuffleDeck(deckOfCards)
 
 	return deckOfCards, nil
+}
+
+// DrawCard ...
+// This method extracts the last card from the current deck of cards
+// Parameters:
+// - deckOfCards: ([]deck.Card) the current deck of cards
+// Return:
+// - (deck.Card) the last card from the deckOfCards (now the deckOfCards will not have that card anymore, since it was drawn out)
+func DrawCard(deckOfCards []deck.Card) deck.Card {
+	card := deckOfCards[len(deckOfCards)-1]
+	deckOfCards = deckOfCards[:len(deckOfCards)-1]
+
+	return card
 }
--- blackjack/blackjack_test.go
+++ blackjack/blackjack_test.go
@@ -23,3 +23,23 @@ func TestGenerateBlackjackDeck(t *testing.T) {
 		t.Errorf("Inconsistency. The new deck should have %d cards. The generated one has %d.", len(genericDeckOfCards)*3, len(blackjackDeckOfCards))
 	}
 }
+
+func TestDrawCard(t *testing.T) {
+	deckOfCards, err := GenerateBlackjackDeck(3)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	numberOfCardsInDeck := len(deckOfCards)
+	lastCard := deckOfCards[numberOfCardsInDeck-1]
+	drawnCard := DrawCard(deckOfCards)
+
+	if len(deckOfCards) != numberOfCardsInDeck {
+		t.Errorf("The deck should have %d after the last card was drawn. Instead, the deck has %d.", numberOfCardsInDeck-1, len(deckOfCards))
+	}
+
+	if lastCard != drawnCard {
+		t.Errorf("The card drawn should have been %v. Instead, the \"DrawCard\" method got %v.", lastCard, drawnCard)
+	}
+}
