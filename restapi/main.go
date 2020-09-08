2a2b6f82a4187dffcd61c2090f746d86fb7c05a9
= <danb.alexandru@gmail.com>
2020-09-08 17:12:51 +0300

    [update]   - added the ability to execute multiple commands (execute more api   requests requentially;

--- restapi/main.go
+++ restapi/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/compute/v1"
@@ -25,12 +27,41 @@ func getComputeService() (context.Context, *compute.Service) {
 	return ctx, computeService
 }
 
+func initCustomTerminal() *bufio.Scanner {
+	fmt.Println("Custom terminal v0.1 started...")
+	fmt.Println()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	return scanner
+}
+
+func runCustomTerminal(scanner *bufio.Scanner) string {
+	fmt.Printf("\n>> ")
+	scanner.Scan() // use `for scanner.Scan()` to keep reading
+	command := scanner.Text()
+
+	return command
+}
+
 func main() {
+
 	ctx, computeService := getComputeService()
 
 	// Project ID for this request.
 	project := "tutorialproject-200903" // TODO: Update placeholder value.
-	result := Execute["gcloud compute accelerator-types list"](ctx, computeService, project)
 
-	fmt.Println(result)
+	scanner := initCustomTerminal()
+	var command string
+	for {
+		command = runCustomTerminal(scanner)
+
+		if function, ok := Execute[command]; ok {
+			result := function(ctx, computeService, project)
+			fmt.Println(result)
+		} else {
+			fmt.Printf("Command not found: \"%s\".\n", command)
+		}
+
+	}
+
 }
